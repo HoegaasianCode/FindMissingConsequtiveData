7b39abef779b0422c5ffeeb78a80938b7b32f2ed
JoakimHoegaas <80755360+HoegaasianCode@users.noreply.github.com>
2023-12-21 19:07:37 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,71 +1,51 @@
-/*
-   TODO:
-       (1): Skriv en funksjon som kan oppgi en samling av manglende id/ints.
-            Mangler Id 10-20, må det skrives opp logikk for en eller annen range.
-*/
 internal class Program
 {
-        private static void Main(string[] args)
+    private static void Main(string[] args)
+    {
+        int nonConsequtiveValue = null;
+        int[] intArrTest = { 9, 2, 4, 5, 6, 7, 1, 8 };
+        IsValidConsequtiveNumber(intArrTest, nonConsequtiveValue);
+        // string path = @"C:\Users\had00bin\Desktop\IdValues.txt";
+        // int[] intArr = GetIntArrFromTextFile(path);
+        // IsValidConsequtiveNumber(intArr, nonConsequtiveValue);
+    }
+    void IsValidConsequtiveNumber(int[] intArr, int nonConsequtiveValue)
+    {
+        Array.Sort(intArr);
+        int lastInt = intArr[0];
+        for (int i = 0; i < intArr.Length; i++)
         {
-            int[] intArr = { 9, 2, 4, 5, 6, 7, 1, 8 };
-            // {1, 2, 4, 5, 6, 7, 9} after Sort()
-            // string path = @"C:\Users\had00bin\Desktop\IdValues.txt";
-            // int[] intArr = GetIntArrFromTextFile(path);
-            List<int> missingIntList = new();
-            IsValidConsequtiveNumber(intArr, missingIntList);
-            // MyTestFunc(GetIntArrFromTextFile(path));
+            if (i == 0) // this case might be avoided by using ++i instead of i++
+            {
+                continue;
+            }
+            int _int = intArr[i];
+            if (_int != lastInt + 1)
+            {
+                nonConsequtiveValue = (_int - 1);
+            }
+            lastInt = _int;
         }
-        void IsValidConsequtiveNumber(int[] intArr, List<int> missingIntList)
+        WriteResultsToConsole(nonConsequtiveValue);
+    }
+    void WriteResultsToConsole(int nonConsequtiveValue)
+    {
+        if (nonConsequtiveValue == null)
         {
-            Array.Sort(intArr);
-            int lastInt = intArr[0];
-            for (int i = 0; i < intArr.Length; i++)
-            {
-                if (i == 0)
-                {
-                    continue;
-                }
-                int _int = intArr[i];
-                if (_int != lastInt + 1)
-                {
-                    missingIntList.Add(_int - 1);
-                }
-                lastInt = _int;
-            }
-            WriteResultsToConsole(missingIntList.ToArray());
+            Console.Write("OK - All values are ordered by increments of 1");
+            return;
         }
-        int[] GetIntArrFromTextFile(string path)
-        {
-            string[] strArr = File.ReadAllLines(path);
-            List<int> intList = new();
-            foreach (string str in strArr)
-            {
-                intList.Add(int.Parse(str));
-            }
-            return intList.ToArray();
-        }
-        void WriteResultsToConsole(int[] missingIntArr)
-        {
-            if (missingIntArr.Length < 1)
-            {
-                Console.Write("OK - All values are ordered by increments of 1");
-                return;
-            }
-            string str = missingIntArr.Length.ToString();
-            Console.WriteLine(str + " missing case(s) found:");
-            foreach (int _int in missingIntArr)
-            {
-                Console.WriteLine(_int.ToString());
-            }
-        }
-        void MyTestFunc(int[] intArr)
-        {
-            foreach (int _int in intArr)
-            {
-                Console.WriteLine(_int.ToString());
-            }
-        }
- }
-/*
-   --- Auxcillary functions go here ---
-*/
+        string str = nonConsequtiveValue.ToString();
+        Console.WriteLine("Missing case at: " + str);
+    }
+}
+int[] GetIntArrFromTextFile(string path)
+{
+    string[] strArr = File.ReadAllLines(path);
+    List<int> intList = new();
+    foreach (string str in strArr)
+    {
+        intList.Add(int.Parse(str));
+    }
+    return intList.ToArray();
+}
